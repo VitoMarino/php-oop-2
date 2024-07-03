4cac61a62c4b92f594e59f66d1eb885835cb903c
VitoMarino <marino.vito2001@gmail.com>
2024-07-03 14:30:33 +0200

    Start

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php 
+require_once __DIR__ . ("/classi/Prodotto.php");
+require_once __DIR__ . ("/classi/Accessorio.php");
+require_once __DIR__ .("/classi/Cibo.php");
+require_once __DIR__ . ("/classi/Gioco.php");
+
+
+
+
+
+?>
