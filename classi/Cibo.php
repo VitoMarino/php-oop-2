b673588d78b2c2ca54bd1eada7582fa3d3f6e16a
VitoMarino <marino.vito2001@gmail.com>
2024-07-03 15:44:47 +0200

    Sottoclasse cibo

--- classi/Cibo.php
+++ classi/Cibo.php
@@ -1,10 +1,45 @@
 <?php 
 require_once __DIR__ . ("/Prodotto.php");
+
+/**
+ * Cibo è una sottoclasse di Prodotto
+ * 
+ */
 class Cibo extends Prodotto{
+    /**
+     *Varibili di istanza
+     */
     private $prezzo;
     private $peso;
     private $ingredienti;
 
+    /**
+     *Costrutto
+     * @param [string] $categoria
+     * @param [string] $prezzo
+     * @param [string] $peso
+     * @param [string] $ingredienti
+     */
+    public function __construct($categoria, $prezzo, $peso, $ingredienti){
+        /**
+         * Ereditarietà del parent $categoria
+         */
+        parent::__construct($categoria);
+        $this->prezzo = $prezzo;
+        $this->peso = $peso;
+        $this->ingredienti = $ingredienti;
+    }
+    public function getPrezzo(){
+        return $this->prezzo;
+    }
+
+    public function getPeso(){
+        return $this->peso;
+    }
+
+    public function getIngredienti(){
+        return $this->ingredienti;
+    }
 
 }
 
--- classi/Prodotto.php
+++ classi/Prodotto.php
@@ -3,32 +3,21 @@
  * Creo una super classe primaria che contiene delle sottoclassi. Ha come variabili di istanza degli animali
  */
 class Prodotto {
-    private $cane;
-    private $gatto;
-    private $uccello;
-    private $pesce;
-
-    public function __construct($cane, $gatto, $uccello, $pesce) {
-        $this->cane = $cane;
-        $this->gatto = $gatto;
-        $this->uccello = $uccello;
-        $this->pesce = $pesce;
+    private $categoria;
+/**
+ *Costrutto $categoria
+ * @param [string] $categoria
+ */
+    public function __construct($categoria) {
+        $this->categoria = $categoria;
     }
 
-    public function getCane(){
-        return $this->cane;
-    }
-
-    public function getGatto(){
-        return $this->gatto;
-    }
-
-    public function getUccello(){
-        return $this->uccello;
-    }
-
-    public function getPesce(){
-        return $this->pesce;
+    /**
+     *Funzione di ritorno
+     * @return $categoria
+     */
+    public function getCategoria(){
+        return $this->categoria;
     }
 }
 
